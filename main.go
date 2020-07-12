3772edb9bca411cddd04397d96f845df1743bb4b
cnpollux <cnpollux@163.com>
2020-07-12 17:37:43 +0800

    add gin

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	g := gin.Default()
+	g.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	g.Run()
+}
